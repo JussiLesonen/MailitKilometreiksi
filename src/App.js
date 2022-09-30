0dd2f5069e16e68abc8034d6c4a42482ef063e42
Jussi <t1leju02@students.oamk.fi>
2022-09-30 09:32:42 +0300

    Projektin alustus.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [mailit, setMailit] = useState(0)
+  const [kilometrit, setKilometrit] = useState(0)
+
+  function laske(e) {
+    e.preventDefault()
+    setKilometrit(mailit*1.609)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h3>Mailit kilometreiksi</h3>
+      <form action="" onSubmit={laske}>
+        <div>
+          <label htmlFor="">Mailit</label>
+          <input type="text" value={mailit} onChange={e => setMailit(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="">Kilometrit</label>
+          <output>{kilometrit.toFixed(2)}</output>
+        </div>
+        <button>Laske</button>
+      </form>
     </div>
   );
 }
